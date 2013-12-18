5894fe66260b6d1d6878e94e132994ab618930e3
brad-hookano <brad@hookano.ca>
2013-12-18 12:28:33 -0700

    )

--- netmap/src/main/java/ca/cybera/netmap/controller/OrganizationController.java
+++ netmap/src/main/java/ca/cybera/netmap/controller/OrganizationController.java
@@ -9,7 +9,6 @@ import javax.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.multipart.MultipartFile;
 
 import ca.cybera.netmap.assembler.OrganizationAssembler;
@@ -48,7 +46,6 @@ public class OrganizationController extends BaseController {
 		log.debug("outgoing: "+ret);
 		
 		return assembler.getDTO(ret);
-
 	}
 
 	@RequestMapping(value = "/{uuid}", method = RequestMethod.DELETE)
